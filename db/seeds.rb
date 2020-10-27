e6a877826d284d3e0282e937fe95b5ea55619670
nohaderf <freda.hon@gmail.com>
2020-10-27 18:13:58 -0400

    commit tree f92c853ac1f328305d87154462c37e8d244b7a5b

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+coupon1 = Coupon.create!(coupon_code: "FREESTUFF", store: "Chipotle")
